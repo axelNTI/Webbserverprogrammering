2d00ec76a50eb7327735b0486f9ef6c83160176b
axelNTI <axel.thornberg@elev.ga.ntig.se>
2024-01-18 11:31:36 +0100

    2024-01-18

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.use(express.static("./web"));
+app.use(express.static("./04"));
 app.listen(3000, () => {
   console.log("Servern körs, besök http://localhost:3000");
 });
